30e9fe5b51079dfcf6037a2042031b4528dc5d31
Kevin Katsura Dani Sitanggang <t-kevin.sitanggang@traveloka.com>
2023-03-25 13:41:58 +0700

    feat: boilerplate

---
+++ src/test/java/com/Dans/DevTest/DevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dans.DevTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
